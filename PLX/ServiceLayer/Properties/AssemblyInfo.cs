171c14805173308a5f5b292c7e644d993de4b8d0
Alexandre <Alexandre@ALEXANDRE-PC>
2016-11-20 16:26:09 -0200

    Correção, services

--- PLX/ServiceLayer/Properties/AssemblyInfo.cs
+++ PLX/ServiceLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("631b33f9-5b99-40c7-aade-497997eb27ce")]
+[assembly: Guid("66ee4d04-8f0b-4d3a-b614-0d582f6516dd")]
 
 // Version information for an assembly consists of the following four values:
 //
