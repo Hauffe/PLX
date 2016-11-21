7e8029c31f032dc356bb2e81e8309556318a85fd
Alexandre <Alexandre@ALEXANDRE-PC>
2016-11-21 01:12:41 -0200

    Global

--- PLX/WebLayerProject/Global.asax.cs
+++ PLX/WebLayerProject/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebLayerProject.App_Start;
 
 namespace WebLayerProject
 {
@@ -16,6 +17,7 @@ namespace WebLayerProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MapperConfig.InicializarMapper();
         }
     }
 }
